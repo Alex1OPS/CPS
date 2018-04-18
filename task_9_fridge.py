88837005c348b627cce6778824873efa216b693c
Alex1OPS <ale51098223@yandex.ru>
2018-04-18 07:29:04 +0600

    Cosmetic changes + start product compatibility accounting

--- task_9_fridge.py
+++ task_9_fridge.py
@@ -14,20 +14,37 @@ E = [3, 2, 10]
 SHELVES_PRODUCT_MATRIX = []
 
 ShelfType = namedtuple("ShelfType", ['x_start', 'x_stop', 'y_start', 'y_stop', 't_min', 't_max', 'e_energy'])
+ProductCat = namedtuple("ProductCat", ['name', 'code'])
+
+# Опишем категории продуктов
+FRUITS = ProductCat(name="Fruits", code=1)
+VEGETABLES = ProductCat(name="Vegetables", code=3)
+MEAT = ProductCat(name="Meat", code=3)
+MILK = ProductCat(name="Milk", code=4)
+FISH = ProductCat(name="Fish", code=5)
+BAKE = ProductCat(name="Bake", code=6)
+
+# Пары несовместимых групп продуктов
+INCOMPATIBLE_GROUPS = (
+    (FRUITS, FISH),
+    (VEGETABLES, FISH),
+    (MEAT, BAKE),
+    (MILK, FISH)
+)
 
 
 # функция возвращает допустимый для продукта интервал внутри полок
 # с подходящей температурой
-def get_allowable_area(shls, ax, temperature):
+def get_allowable_area(shls, ax_attr, temperature):
     f_ = CpoStepFunction()
-    if ax == 'x':
+    if ax_attr == 'x':
         f_.set_value(0, SIZE_SHELF["x"] * SHELF_COUNT, 100)
         for ih in range(len(shls)):
             f_.set_value(shls[ih].x_start, shls[ih].x_start + 1, 0)
             f_.set_value(shls[ih].x_stop - 1, shls[ih].x_stop, 0)
             if not shls[ih].t_min <= temperature <= shls[ih].t_max:
                 f_.set_value(shls[ih].x_start, shls[ih].x_stop, 0)
-    elif ax == 'y':
+    elif ax_attr == 'y':
         f_.set_value(0, SIZE_SHELF["y"] * SHELF_COUNT, 100)
         for ih in range(len(shls)):
             f_.set_value(shls[ih].y_start, shls[ih].y_start + 1, 0)
@@ -38,23 +55,13 @@ def get_allowable_area(shls, ax, temperature):
 
 
 # получение затрат в Дж на охлаждение 1 кг продукта
-def get_energy(shls, mass, pos):
-    en_ = 0
-    for ih in range(len(shls)):
-        if shls[ih].x_start <= pos <= shls[ih].x_stop:
-            en_ = shls[ih].e_energy
-            break
-    return mass * en_
-
-
 def get_prepared_energy(pos, product_ind):
     return SHELVES_PRODUCT_MATRIX[pos][product_ind]
+
 
 # -----------------------------------------------------------------------------
 # Подготовка данных
 # -----------------------------------------------------------------------------
-
-
 # Создадим набор полок с характеристиками
 SHELVES = []
 for i in range(SHELF_COUNT):
@@ -63,29 +70,33 @@ for i in range(SHELF_COUNT):
     SHELVES.append(o)
 
 # Размеры продуктов
-WEIGHT_SIZE_A = [25, 17, 16, 15, 11, 9, 8, 7, 6, 4, 2, 20, 20, 5, 60
-                 ]
-WEIGHT_SIZE_B = [10, 15, 2, 4, 8, 9, 3, 5, 2, 3, 1, 20, 20, 30, 40
-                 ]
-PRODUCT_T = [0, 7, 11, 2, 3, 8, 5, 12, 2, 3, 1, 1, 1, 1, 1
-             ]
-PRODUCT_WEIGHT = [2, 7, 11, 2, 3, 8, 5, 12, 2, 3, 1, 1, 1, 1, 1
-             ]
+WEIGHT_SIZE_A = [25, 17, 16, 15, 11,
+                 9, 8, 7, 6, 4,
+                 2, 20, 20, 5, 60]
+WEIGHT_SIZE_B = [10, 15, 2, 4, 8,
+                 9, 3, 5, 2, 3,
+                 1, 20, 20, 30, 40]
+PRODUCT_T = [0, 7, 11, 2, 3,
+             8, 5, 12, 2, 3,
+             1, 1, 1, 1, 1]
+PRODUCT_WEIGHT = [2, 7, 11, 2, 3,
+                  8, 5, 12, 2, 3,
+                  1, 1, 1, 1, 1]
 PRODUCT_NAMES = ["m" + str(i) for i in range(len(WEIGHT_SIZE_A))]
+PRODUCT_CATS = [FRUITS, FRUITS, FRUITS, FRUITS, FRUITS,
+                FRUITS, FRUITS, FRUITS, FRUITS, FRUITS,
+                FRUITS, FISH, FRUITS, FRUITS, FRUITS]
 NB_WEIGHTS = len(WEIGHT_SIZE_A)
-print(len(WEIGHT_SIZE_A) == len(WEIGHT_SIZE_B) == len(PRODUCT_T))
 
-# for i in range(SIZE_SHELF["x"] * SHELF_COUNT):
-#     p = []
-#     for j in range(len(PRODUCT_WEIGHT)):
-#         p.append(E[int(i / SIZE_SHELF["x"])] * PRODUCT_WEIGHT[j])
-#     SHELVES_PRODUCT_MATRIX.append(p)
+if not len(WEIGHT_SIZE_A) == len(WEIGHT_SIZE_B) == len(PRODUCT_T):
+    raise RuntimeError("Не совпадают размерности матриц продуктов!")
 
 for i in range(len(PRODUCT_WEIGHT)):
     p = []
     for j in range(SIZE_SHELF["x"] * SHELF_COUNT):
         p.append(E[int(j / SIZE_SHELF["x"])] * PRODUCT_WEIGHT[i])
     SHELVES_PRODUCT_MATRIX.append(p)
+
 # -----------------------------------------------------------------------------
 # Описание модели
 # -----------------------------------------------------------------------------
@@ -101,8 +112,6 @@ vy = [mdl.interval_var(size=WEIGHT_SIZE_B[i], name="Y" + str(i), end=(0, SIZE_SH
 for i in range(NB_WEIGHTS):
     mdl.add(mdl.forbid_extent(vx[i], get_allowable_area(SHELVES, 'x', PRODUCT_T[i])))
 
-# for i in range(NB_WEIGHTS):
-#     mdl.add(mdl.forbid_extent(vy[i], get_allowable_area(SHELVES, 'y', PRODUCT_T[i])))
 
 # Запретим пересечение продуктов
 for i in range(NB_WEIGHTS):
@@ -110,7 +119,7 @@ for i in range(NB_WEIGHTS):
         mdl.add((mdl.end_of(vx[i]) <= mdl.start_of(vx[j])) | (mdl.end_of(vx[j]) <= mdl.start_of(vx[i]))
                 | (mdl.end_of(vy[i]) <= mdl.start_of(vy[j])) | (mdl.end_of(vy[j]) <= mdl.start_of(vy[i])))
 
-# mdl.add(mdl.minimize(mdl.sum(get_energy(shls=SHELVES, mass=PRODUCT_WEIGHT[t], pos=mdl.start_of(vx[t])) for t in range(len(vx)))))
+# OF - минимум энергии, необходимой для поддержания температурных режимов продуктов на полках
 mdl.add(mdl.minimize(
     mdl.sum(
         mdl.element(SHELVES_PRODUCT_MATRIX[t], mdl.start_of(vx[t])) for t in range(len(vx))
@@ -120,9 +129,8 @@ mdl.add(mdl.minimize(
 # -----------------------------------------------------------------------------
 # Решение solver'ом и вывод
 # -----------------------------------------------------------------------------
-
 print("Solving model....")
-msol = mdl.solve(FailLimit=100000000, TimeLimit=100)
+msol = mdl.solve(FailLimit=100000000, TimeLimit=100, LogVerbosity="Terse")  # use "Quiet" or "Terse"
 print("Solution: ")
 msol.print_solution()
 
